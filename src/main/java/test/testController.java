88ea753ee557323ce2767be1fe53c3c34d62d35e
hadesyi <hadesleeq@gmail.com>
2016-01-20 10:33:25 +0900

    Initial commit

---
+++ src/main/java/test/testController.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Project: wcl
+ * FileName: testController
+ * Date: 2016-01-20
+ * Time: 오전 10:31
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+public class testController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "hello spring boot!";
+    }
+}
---
+++ src/main/java/tk/hadeslee/WclApplication.java
@@ -0,0 +1,12 @@
+package tk.hadeslee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WclApplication.class, args);
+	}
+}
